2f9105e1b318b44d0c7ffe825cbb74d751c25faa
treeform <starplant@gmail.com>
2010-09-04 22:26:34 -0700

    added css and jquery include played around with ui styling

--- supttt.py
+++ supttt.py
@@ -1,13 +1,24 @@
 import tornado.httpserver
 import tornado.ioloop
 import tornado.web
+from jinja2 import Template
 
 class MainHandler(tornado.web.RequestHandler):
+    
     def get(self):
-        self.write("Hello, world")
+        BOARD = Template(open('templates/board.html').read())
+        self.write(BOARD.render(world="tic tac toe"))
+
+class StyleCSS(tornado.web.RequestHandler):
+    
+    def get(self):
+        self.set_header("Content-Type", "text/css")
+        STYLE = open('data/style.css').read()
+        self.write(STYLE)
 
 application = tornado.web.Application([
     (r"/", MainHandler),
+    (r"/style.css", StyleCSS),
 ])
 
 if __name__ == "__main__":
